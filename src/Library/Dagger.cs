a11948436b3bdffc64df59d0363d1a8d66f214ff
jooac <joacosan2212@gmail.com>
2026-04-27 18:54:30 -0300

    Initialize Elve

---
+++ src/Library/Dagger.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Ucu.Poo.RolePlayGame
+{
+    /// <summary>
+    /// Representa un item de ataque Daga, aporta
+    /// un valor de daño de ataque del personaje
+    /// </summary>
+    public class Dagger
+    {
+        public int Damage {get; set;}
+        public Dagger( int damage)
+        {
+            this.Damage = damage ;
+        }
+    }
+}
